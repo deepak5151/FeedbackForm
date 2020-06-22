c35cdf2241f667fba7d3408a8ac68ac5832c8f62
Deepak Kapoor <nk93555@gmail.com>
2020-06-22 10:33:38 +0530

    Updates

---
+++ precache-manifest.242458388bc4a4cb5823005f9457b11a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f59a0dc50704652459669e32fcb4728b",
+    "url": "/FeedbackForm/index.html"
+  },
+  {
+    "revision": "0943e383f4e38ba9c970",
+    "url": "/FeedbackForm/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5fcf5d4cea41d51cb216",
+    "url": "/FeedbackForm/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0943e383f4e38ba9c970",
+    "url": "/FeedbackForm/static/js/2.9908d950.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/FeedbackForm/static/js/2.9908d950.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fcf5d4cea41d51cb216",
+    "url": "/FeedbackForm/static/js/main.2f858ade.chunk.js"
+  },
+  {
+    "revision": "9d201463c4404dd227bf",
+    "url": "/FeedbackForm/static/js/runtime-main.3d3bf7f3.js"
+  }
+]);
